62915949f3795b90db8867368a602c279e0a4708
SparklinggeyeS <112405306+krishnaomre@users.noreply.github.com>
2023-11-06 21:26:27 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,38 @@
+ var numbers=[1,2,3,4,5];
+ console.log(numbers);
+
+
+  var len=numbers.push(6);
+  console.log(numbers);
+
+ var lastvalue = numbers.pop();
+ console.log(numbers);
+
+ numbers.unshift(100);
+ console.log(numbers);
+
+
+ 
+ numbers.shift();
+ numbers.shift();
+ console.log(numbers);
+
+  numbers.slipe(1,3);
+ console.log(numbers);
+  numbers.splice(1);
+ numbers.splice(2,2,400);
+ console.log(numbers);
+  var arr1=[1,2,3,4];
+ console.log(arr1);
+ var arr2=[5,6,7];
+ console.log(arr2)
+ var arr =arr1.concat(arr2);
+
+var arr =arr2.concat(arr1);
+ console.log(arr);
+ console.log(arr1.join('+'));
+console.log(arr1.join('*'));
+
+
+
+
